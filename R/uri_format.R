7438ceadd827e0a8a54755ae532d040a9bf2bd91
Carl Boettiger <cboettig@gmail.com>
2018-11-24 20:39:54 -0800

    ugh-ly

--- R/uri_format.R
+++ R/uri_format.R
@@ -5,6 +5,7 @@ uri_format <- function(string, prefix = NULL){
       if (!grepl(":$", prefix)) prefix <- (paste0(prefix, ":"))
       string <- paste0(prefix, string)
     } else if (grepl(":", prefix)){
+      if(!grepl("\\w+:/*\\w", string)) # don't prefix already prefixed terms
       string <- paste0(prefix, string)
     }
   }
